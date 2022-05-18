9e31517215fe2944cd004543cd7358a9e33dc103
grbd <hecatrons.workshop@gmail.com>
2022-05-18 23:41:34 +0100

    initial processing

--- src/LogMerger/Merger/LogFile.cs
+++ src/LogMerger/Merger/LogFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LogMerger.Merger {
+﻿namespace LogMerger.Merger {
 
     /// <summary> A representation of a log file being read in. </summary>
     public class LogFile {
@@ -13,10 +7,57 @@ namespace LogMerger.Merger {
         /// <value> The full pathname of the file. </value>
         public string FilePath { get; set; }
 
+        /// <summary> Stream reader for the file. </summary>
+        /// <value> The stream reader. </value>
+        private StreamReader _Reader { get; set; }
+
+        /// <summary> Current log line in the source file. </summary>
+        /// <value> Current log line in the source file. </value>
+        public string CurrentLine { get; set; }
+
+        /// <summary> Current dttm of the log entry line. </summary>
+        /// <value> The current dttm. </value>
+        public DateTime CurrentDttm { get; set; }
+
+        /// <summary> If the end of the file has been reached. </summary>
+        /// <value> True if end of file, false if not. </value>
+        public bool EndOfFile { get; set; }
+
         /// <summary> Constructor. </summary>
         /// <param name="filePath"> Full pathname of the file. </param>
         public LogFile(string filePath) {
             FilePath = filePath;
+            _Reader = new StreamReader(filePath);
+            CurrentLine = "";
+            EndOfFile = false;
+        }
+
+        /// <summary> Reads the next line. </summary>
+        public void ReadLine() {
+            if (EndOfFile)
+                return;
+            var line = _Reader.ReadLine();
+            if (line != null && line != "") {
+                CurrentLine = line;
+                ParseLine();
+            } else {
+                EndOfFile = true;
+                CurrentLine = "";
+            }
+        }
+
+        /// <summary> Resets the streamreader. </summary>
+        public void Reset() {
+            _Reader = new StreamReader(FilePath);
+            EndOfFile = false;
+        }
+
+        /// <summary> Parse the read in line. </summary>
+        private void ParseLine() {
+            // Read the initial dttm for sorting
+            var dttmformat = "yyyy/MM/dd HH:mm:ss";
+            var dttmstr = CurrentLine.Substring(0, dttmformat.Length);
+            CurrentDttm = DateTime.ParseExact(dttmstr, dttmformat, null);
         }
     }
 }
--- src/LogMerger/Merger/LogMerger.cs
+++ src/LogMerger/Merger/LogMerger.cs
@@ -11,11 +11,16 @@
         /// <value> The source logfiles. </value>
         private List<LogFile> _logfiles { get; set; }
 
+        /// <summary> StreamWriter for writing to the destination. </summary>
+        /// <value> The stream writer. </value>
+        private StreamWriter _writer { get; set; }
+
         /// <summary> Constructor. </summary>
         /// <param name="opts"> Merger options. </param>
         public LogMerger(LogMergerOpts opts) {
             _opts = opts;
             _logfiles = new List<LogFile>();
+            _writer = new StreamWriter(_opts.DestFilePath);
         }
 
         /// <summary> Starts the merge. </summary>
@@ -29,8 +34,40 @@
                 _logfiles.Add(new LogFile(file));
             }
 
+            // Do an initial read line on all log sources
+            foreach (var item in _logfiles) {
+                item.ReadLine();
+            }
 
-            // TODO
+            while(LogsAvailable()) {
+
+                // TODO
+                foreach (var file in _logfiles) {
+                    while(file.EndOfFile == false) {
+                        file.ReadLine();
+                        if (file.EndOfFile == false) {
+                            _writer.WriteLine(file.CurrentLine);
+                        }
+                    }
+                }
+
+            }
+            _writer.Close();
+        }
+
+        /// <summary> Determine if all the source files have finished reading. </summary>
+        /// <returns> True if log data still available. </returns>
+        private bool LogsAvailable() {
+            var ret = false;
+            foreach (var item in _logfiles) {
+                // If at least one of the source files hasn't finished reading
+                // Then make sure to carry on
+                if (item.EndOfFile == false) {
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
         }
     }
 }
